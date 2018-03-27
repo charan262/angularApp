2b1d42a63b6da67b57806921c9b8374ad26e39af
Raj262 <raj.webpassion@gmail.com>
2018-03-27 14:23:47 -0500

    add routing using @angular/router

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,11 +7,14 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'test desciption', 'Hello', [new Ingredient('meat',1)]),
-        new Recipe('A test recipe', 'test desciption', 'Hello', [new Ingredient('fries',2)])
+        new Recipe('Burger', 'Awesome', 'Hello', [new Ingredient('meat',1)]),
+        new Recipe('Pizza', 'Nice', 'Hello', [new Ingredient('fries',2)])
       ]
       getRecipe() {
           return this.recipes.slice();
+      }
+      getRecipes (index: number) {
+        return this.recipes.slice()[index]
       }
       constructor(private slService: ShoppingListService){}
       addIngredientsToShoppingList(ingredients: Ingredient[]){
